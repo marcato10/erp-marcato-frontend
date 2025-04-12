c810fe0772cd7bcc184f9be9d31f8f97c7c1c8dd
marcato <lucasmarcato10@hotmail.com>
2025-04-12 17:10:38 -0300

    First commit

---
+++ src/components/Home.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import LoginItem from './Login.vue'
+
+
+</script>
+
+<template>
+  <LoginItem>
+    
+  </LoginItem>
+</template>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import HomeComponent from '../components/Home.vue'
+import { RouterLink, RouterView } from 'vue-router'
+
+</script>
+
+<template>
+  <main>
+    <HomeComponent>
+      <RouterLink to="/login">Fazer Login</RouterLink>
+      <RouterLink to="/register">Fazer Registro</RouterLink>
+    </HomeComponent>
+    
+  </main>
+</template>
